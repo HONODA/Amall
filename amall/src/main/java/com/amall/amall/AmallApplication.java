a38ed78ecce759ef496f70d03bfba4268da823f1
香芋解百纳 <756617901@qq.com>
2020-12-21 18:32:02 +0800

    初始化AmallMaven项目

---
+++ amall/src/main/java/com/amall/amall/AmallApplication.java
@@ -0,0 +1,13 @@
+package com.amall.amall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmallApplication.class, args);
+	}
+
+}
